81418d03e179e07d9772c68074c19f1fd5bc4ec8
Agrim Gupta <agrimgupt195@gmail.com>
2025-09-16 13:12:19 +0530

    add host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, 
+    port: Number(process.env.PORT) || 5173,
+  },
 })
